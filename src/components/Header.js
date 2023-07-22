969a5eeccf35d501ec208e66011ba67d7c644a99
QadirDeveloper <126903408+QadirDeveloper@users.noreply.github.com>
2023-07-21 22:32:27 -0700

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,261 @@
+import "./Header.css"
+import React from 'react'
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+
+export default function Assignment() {
+  return (
+    <div>
+      
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+    crossOrigin="anonymous"
+  />
+  <link rel="stylesheet" href="style.css" />
+  <div className="body">
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-4 col-sm-12">
+        <h1 className="one">Discover digital art &amp; Collect NFTs</h1>
+        <p className="one">
+          NFT marketplace UI created with Anima for Figma. Collect, buy and sell
+          art from more than 20k NFT artists.
+        </p>
+        <button className="butt2">Get Started</button>
+      </div>
+      <div className="col-6">
+        <img
+          className="img1"
+          src="./images/Highlighted NFT.png"
+          alt="not found"
+        />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-1">
+        <h6 className="one">240k+</h6>
+        <p className="one">Total Sale</p>
+      </div>
+      <div className="col-1">
+        <h6 className="one">100k+</h6>
+        <p className="one">Auctions</p>
+      </div>
+      <div className="col-1">
+        <h6 className="one">240k+</h6>
+        <p className="one">Artists</p>
+      </div>
+    </div>
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+  {/* 2nd oart is start */}
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h2 className="one">Trending Collection</h2>
+        <p className="one">Checkout our weekly updated trending collection.</p>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <img src="./images/Collection Card.png" alt="not found" />
+      </div>
+      <div className="col">
+        <img src="./images/Collection Card4.png" alt="not found" />
+      </div>
+      <div className="col">
+        <img src="./images/Collection Card (1).png" alt="not found" />
+      </div>
+    </div>
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  {/* !st cards are complete */}
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h2 className="one">Top creators</h2>
+        <p className="one">
+          Checkout Top Rated Creators on the NFT Marketplace
+        </p>
+      </div>
+      <div className="col">
+        <button className="butt">View Ranking</button>
+      </div>
+    </div>
+  </div>
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card11.png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (1).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (2).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card 14.png" alt="not found" />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (3).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (4).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (5).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (6).png" alt="not found" />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (7).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card 13.png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card (8).png" alt="not found" />
+      </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+        <img src="./images/Artist Card 12.png" alt="not found" />
+      </div>
+    </div>
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+  {/* 12 images tag are complete */}
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h2 className="one">Discover More NFTs</h2>
+        <p className="one">Explore new trending NFTs</p>
+      </div>
+      <div className="col">
+        <button className="butt">
+          {" "}
+          <img src="./images/download.png" alt="" /> See All
+        </button>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <img src="./images/NFT Card.png" alt="not found" />
+      </div>
+      <div className="col">
+        <img src="./images/NFT Card2.png" alt="not found" />
+      </div>
+      <div className="col">
+        <img src="./images/NFT Card (1).png" alt="not found" />
+        <p>AstroFiction</p>
+      </div>
+    </div>
+  </div>
+  {/* images and icon section start */}
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h2 className="one">Browse Categories</h2>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-2"></div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img  src="./images/Category Card.png" alt="not found" />
+      </div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card 1.png" alt="not found" />
+      </div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card2.png" alt="not found" />
+      </div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card (1).png" alt="not found" />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-2"></div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card (2).png" alt="not found" />
+      </div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card (4).png" alt="not found" />
+      </div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card (5).png" alt="not found" />
+      </div>
+      <div className="col-lg-2 col-sm-6 col-md-4">
+        <img src="./images/Category Card (6).png" alt="not found" />
+      </div>
+    </div>
+  </div>
+
+  <img src="./images/Capture.PNG" alt="" />
+  <h2 className="one">How it Work</h2>
+        <p className="one">Find out how to Get Started</p>
+  <div className="container-flow">
+    <div className="row">
+      <div className="col">
+       
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-4 col-sm-12 col-md-6">
+        <img src="./images/dfd.PNG" alt="" />
+      </div>
+      <div className="col-lg-4 col-sm-12 col-md-6">
+        <img src="./images/fvg.PNG" alt="" />
+      </div>
+      <div className="col-lg-4 col-sm-12 col-md-6">
+        <img src="./images/njm.PNG" alt="" />
+      </div>
+    </div>
+  <div className="container">  
+    <div className="row">
+      <div className="col">
+        <div className="Card">
+          <img src="./images/bgb.PNG" alt="" />
+        </div>
+      </div>
+      <div className="col-lg-4 col-sm-12">
+        <h2 className="one">Join Our weekly Didest</h2>
+        <p className="one">
+          Get Exclusive Promotion &amp; Update Stright to Your Inbox.
+        </p>
+        <input type="Enter Your Email Here" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  </div>
+  
+  {/* all deta is complete here */}
+  </div>
+    </div>
+    </div>
+    
+  )
+}
